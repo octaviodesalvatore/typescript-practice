95c5035bcc256df940462a1a9907c38d3ff1100c
Octavio <octaviodesalvatore@gmail.com>
2022-05-13 15:01:50 -0300

    input data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./App.css";
+import { ITask } from "./Interfaces";
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = {
+      taskName: task,
+      deadline: deadline,
+    };
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
+  };
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Tarea..." />
-          <input type="number" placeholder="Tiempo limite (Días) ..." />
+          <input
+            type="text"
+            placeholder="Tarea..."
+            onChange={handleChange}
+            name="task"
+          />
+          <input
+            type="number"
+            placeholder="Tiempo limite (Días) ..."
+            onChange={handleChange}
+            name="deadline"
+          />
         </div>
-        <button>Añadir tarea</button>
+        <button onClick={addTask}>Añadir tarea</button>
       </div>
       <div className="todoList"></div>
     </div>
