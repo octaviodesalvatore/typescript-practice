25ed31849092b8fae549ed57a78735bf04632092
Octavio <octaviodesalvatore@gmail.com>
2022-05-13 14:32:08 -0300

    Adding inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Tarea..." />
+          <input type="number" placeholder="Tiempo limite (Días) ..." />
+        </div>
+        <button>Añadir tarea</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
